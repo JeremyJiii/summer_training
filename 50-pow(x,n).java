4b2c0aee3ba4a79207e994ffd5eb24d73932f4de
JeremyJiii <jixiangyu0124@gmail.com>
2018-07-02 22:24:16 +0800

    Update 50-pow(x,n).java

--- 50-pow(x,n).java
+++ 50-pow(x,n).java
@@ -2,7 +2,7 @@ class Solution {
     public double myPow(double x, int n) {
         if(n==0) return 1;
         double t = myPow(x,n/2);
-        if(n%2!=0) return n<0 ? 1/x*t*t : x*t*t; # the last of odd term and n==1
+        if(n%2!=0) return n<0 ? 1/x*t*t : x*t*t; // the last of odd term and n==1
         else return t*t;
     }
 }
